abd56a06855adee25f56d9e27bb6d772d1d5b5c3
Nicolaas Nijman <nicolaas@nijman.co>
2024-12-07 20:43:49 +0100

    EM540 TCP Client

--- carlo_gavazzi.py
+++ carlo_gavazzi.py
@@ -24,6 +24,14 @@ phase_configs = [
     '3P',
 ]
 
+nr_phases_em540 = [ 3, 3, 2 ]
+
+phase_configs_em540 = [
+    '3P.n',
+    '3P',
+    '2P',
+]
+
 switch_positions = [
     'kVARh',
     '2',
@@ -93,6 +101,68 @@ class EM24_Meter(device.CustomName, device.EnergyMeter):
         super().dbus_write_register(reg, path, val)
         self.sched_reinit()
 
+class EM540_Meter(device.CustomName, device.EnergyMeter):
+    vendor_id = 'cg'
+    vendor_name = 'Carlo Gavazzi'
+    productid = 0xb017
+    productname = 'Carlo Gavazzi EM540 Ethernet Energy Meter'
+    min_timeout = 0.5
+
+    def phase_regs(self, n):
+        s = 2 * (n - 1)
+        return [
+            Reg_s32l(0x0000 + s, '/Ac/L%d/Voltage' % n,        10, '%.1f V'),
+            Reg_s32l(0x000c + s, '/Ac/L%d/Current' % n,      1000, '%.1f A'),
+            Reg_s32l(0x0012 + s, '/Ac/L%d/Power' % n,          10, '%.1f W'),
+            Reg_s32l(0x0040 + s, '/Ac/L%d/Energy/Forward' % n, 10, '%.1f kWh'),
+        ]
+
+    def device_init(self):
+        self.info_regs = [
+            Reg_ver( 0x0302, '/HardwareVersion'),
+            Reg_ver( 0x0304, '/FirmwareVersion'),
+            Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs_em540, write=(0, 4)),
+            Reg_text(0x5000, 7, '/Serial'),
+        ]
+
+        # make sure application is set to H
+        appreg = Reg_u16(0xa000)
+        if self.read_register(appreg) != 7:
+            self.write_register(appreg, 7)
+
+            # read back the value in case the setting is not accepted
+            # for some reason
+            if self.read_register(appreg) != 7:
+                self.log.error('%s: failed to set application to H', self)
+                return
+
+        self.read_info()
+
+        phases = nr_phases_em540[int(self.info['/PhaseConfig'])]
+
+        regs = [
+            Reg_s32l(0x0028, '/Ac/Power',          10, '%.1f W'),
+            Reg_u16( 0x0033, '/Ac/Frequency',      10, '%.1f Hz'),
+            Reg_s32l(0x0034, '/Ac/Energy/Forward', 10, '%.1f kWh'),
+            Reg_s32l(0x004e, '/Ac/Energy/Reverse', 10, '%.1f kWh'),
+        ]
+
+        if phases == 3:
+            regs += [
+                Reg_mapu16(0x0032, '/PhaseSequence', { 0: 0, 0xffff: 1 }),
+            ]
+
+        for n in range(1, phases + 1):
+            regs += self.phase_regs(n)
+
+        self.data_regs = regs
+        self.nr_phases = phases
+
+    def dbus_write_register(self, reg, path, val):
+        super().dbus_write_register(reg, path, val)
+        self.sched_reinit()
+
+
 models = {
     1648: {
         'model':    'EM24DINAV23XE1X',
@@ -117,6 +187,10 @@ models = {
     1653: {
         'model':    'EM24DINAV53XE1PFB',
         'handler':  EM24_Meter,
+    },
+    1760: {
+        'model':    'EM540DINAV23XS1X',
+        'handler':  EM540_Meter,
     },
 }
 
